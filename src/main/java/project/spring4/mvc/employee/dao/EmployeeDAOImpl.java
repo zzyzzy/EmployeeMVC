d23dbd8b77bfaddfe1f66ba8ecfc2147963bc890
zzyzigy <zzyzigy@gmail.com>
2023-04-26 11:20:49 +0900

    dao 클래스 구현

--- src/main/java/project/spring4/mvc/employee/dao/EmployeeDAOImpl.java
+++ src/main/java/project/spring4/mvc/employee/dao/EmployeeDAOImpl.java
@@ -1,16 +1,26 @@
 package project.spring4.mvc.employee.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import project.spring4.mvc.employee.model.Employee;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository("empdao")
 public class EmployeeDAOImpl implements EmployeeDAO {
 
     private JdbcTemplate jdbcTemplate;
+
+    @Value("") private String insertSQL;
+    @Value("#{jdbc['selectSQL']}") private String selectSQL;
+    @Value("") private String selectOneSQL;
+    @Value("") private String updateSQL;
+    @Value("") private String deleteSQL;
 
     @Autowired
     public EmployeeDAOImpl(JdbcTemplate jdbcTemplate) {
@@ -24,7 +34,18 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> selectEmployee() {
-        return null;
+        RowMapper<Employee> mapper = new EmployeeMapper();
+
+        return jdbcTemplate.query(selectSQL, mapper);
+    }
+
+    private class EmployeeMapper implements RowMapper<Employee> {
+        @Override
+        public Employee mapRow(ResultSet rs, int num) throws SQLException {
+            Employee emp = new Employee(rs.getInt(1), rs.getString(2),
+                    rs.getString(3), rs.getString(4), rs.getInt(5));
+            return emp;
+        }
     }
 
     @Override
@@ -41,4 +62,6 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     public int deleteEmployee(int empid) {
         return 0;
     }
+
+
 }
